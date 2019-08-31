aca56b6962a900b5f0189c76c19a3e0f90056a27
fritzgt <tuanis@live.com>
2019-08-31 04:45:11 +0300

    completed registration request with bcrypt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,14 +1,25 @@
 const router = require('express').Router();
 
 //connection to db
-const db = require('../');
+const db = require('../model/users-model');
 
 //importing bcrypt to hash user password
 const bcrypt = require('bcryptjs');
 
 //REGISTER user
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  //get the credentials being pass in the body
+  const creds = req.body;
+  //passing password to bcrypt to hash it
+  const hash = bcrypt.hashSync(creds.password, 14);
+  //setting the password equal to the hash version
+  creds.password = hash;
+  try {
+    const user = await db.add(creds);
+    res.status(201).json({ message: 'Registration successeful!' });
+  } catch {
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 router.post('/login', (req, res) => {
