784b2c6e88e928a22f05fce1e52f4b5aed5ce948
fritzgt <tuanis@live.com>
2019-08-31 06:11:01 +0300

    completed the first two test for registration

--- api/server.spec.js
+++ api/server.spec.js
@@ -4,11 +4,33 @@ const request = require('supertest');
 //importing server
 // const server = require('../api/server');
 const db = require('../database/dbConfig');
-// const users = require('../model/user-model.js');
+const users = require('../model/users-model.js');
 
 //check if evirement is set to testing
 describe('server.js', () => {
   it('Should set the environment testing', () => {
     expect(process.env.DB_ENV).toBe('testing');
+  });
+});
+
+//check register user
+describe('POST /', () => {
+  //clear entries in db before test
+  beforeEach(async () => {
+    await db('users').truncate();
+  });
+  //check if the insert request is working
+  it('Should return 3 users', async () => {
+    await users.add({ username: 'Kristen', password: '1234' });
+    await users.add({ username: 'sasha', password: '1234' });
+    const Users = await db('users');
+    expect(Users).toHaveLength(2);
+  });
+  //checks if the insert comes from this and not for existing data
+  it('Should return 3 users', async () => {
+    let newUser = await users.add({ username: 'Kristen', password: '1234' });
+    expect(newUser.username).toBe('Kristen');
+    newUser = await users.add({ username: 'sasha', password: '1234' });
+    expect(newUser.username).toBe('sasha');
   });
 });
